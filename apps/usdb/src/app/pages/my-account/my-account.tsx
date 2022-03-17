af6f7145cdd512b1872af3136fa634c74437436f
Cory Sougstad <cory@hau.me>
2022-03-16 21:39:25 -0700

    Add my account page

---
+++ apps/usdb/src/app/pages/my-account/my-account.tsx
@@ -0,0 +1,14 @@
+import './my-account.module.scss';
+
+/* eslint-disable-next-line */
+export interface MyAccountProps {}
+
+export function MyAccount(props: MyAccountProps) {
+  return (
+    <div>
+      <h1>Welcome to MyAccount!</h1>
+    </div>
+  );
+}
+
+export default MyAccount;
