8d5ead65e592c7e4636d6789e7ff0992ea055eb5
Cory Sougstad <cory@hau.me>
2022-03-19 14:08:18 -0700

    Convert repetative content blocks to reusable component. Give chart better responsive actions.

--- apps/usdb/src/app/pages/trad-fi/graph/graph.tsx
+++ apps/usdb/src/app/pages/trad-fi/graph/graph.tsx
@@ -2,20 +2,25 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../../store";
 import tradFiGraphLight from '../../../../assets/images/trad-fi-graph.svg';
 import tradFiGraphDark from '../../../../assets/images/trad-fi-graph-dark.svg';
-import { HTMLAttributes } from "react";
+import { Box, Paper, SxProps, Theme } from "@mui/material";
+import style from './graph.module.scss';
 
 /* eslint-disable-next-line */
 export interface LogoProps {
-  style?: HTMLAttributes<HTMLDivElement>
+  style?: SxProps<Theme>
 }
 
 export const Graph = (props: LogoProps): JSX.Element => {
   const themeType = useSelector((state: RootState) => state.app.theme);
 
   return (
-    <div style={{...props.style}}>
-      <img src={themeType === 'light' ? tradFiGraphLight : tradFiGraphDark} alt="Graph showing APR for various investments"/>
-    </div>
+    <Box maxWidth="md" sx={{...props.style, my: '2em'}}>
+      <Paper className={`${style['graphContainer']} ${style['lightBG']}`} sx={{padding: {xs: '2em 1em', md: '4em 3em'}}} elevation={0}>
+        <Box sx={{width: '100%'}}>
+          <img src={themeType === 'light' ? tradFiGraphLight : tradFiGraphDark} className={style['graphImg']} alt="Graph showing APR for various investments"/>
+        </Box>
+      </Paper>
+    </Box>
   );
 }
 
--- apps/usdb/src/app/pages/trad-fi/trad-fi.tsx
+++ apps/usdb/src/app/pages/trad-fi/trad-fi.tsx
@@ -10,21 +10,33 @@ import shield from '../../../assets/icons/shield.svg';
 import Faq from '../../components/faq/faq';
 import Logo from '../../components/logo/logo';
 import Graph from './graph/graph';
+import Headline from '../../components/headline/headline';
 
 /* eslint-disable-next-line */
 export interface TradfiProps {}
 
 export const TradFi = (props: TradfiProps): JSX.Element => {
+  const heroContent = {
+    hero: true,
+    title: "Take your investing to the next level",
+    subtitle: ["The safest way to earn up to 32.5% on your stables.", "No risk. No surprises."]
+  };
+  const simpleSafe = {
+    title: "Simple & safe returns",
+    subtitle: ["USDB offers the safest way to earn up to 20% on your stables. No risk. No surprises."]
+  };
+  const getStarted = {
+    title: "Get started today",
+    subtitle: ["Lorem ipsum dolor sit amet, conctetur adipiscing elit. Etiam auctor commodo."]
+  };
   return (
     <>
       <Box sx={{
         paddingTop: '300px'
       }}
-      className={`${style['hero']} flexCenterCol`}
+      className={`flexCenterCol`}
     >
-      <h1 className={style['heroH1']}>Take your investing to the next level</h1>
-      <Typography variant="h2">The safest way to earn up to 32.5% on your stables.</Typography>
-      <Typography variant="h2">No risk. No surprises.</Typography>
+      <Headline hero={heroContent.hero}  title={heroContent.title} subtitle={heroContent.subtitle} />
       <a href="/trad-fi#get-started">
         <Button sx={{marginTop: '55px', px: '3em', py: '1em'}} variant="outlined">
           Get started 
@@ -32,13 +44,10 @@ export const TradFi = (props: TradfiProps): JSX.Element => {
         </Button>
       </a>
     </Box>
-    <Box className={`${style['tradFiBlock']} flexCenterCol`}>
+    <Box className={`flexCenterCol`}>
       <Box sx={{height: '20em'}}/>
-      <Typography variant="h1">Simple & safe returns</Typography>
-      <Typography variant="h2" maxWidth="sm" sx={{textAlign: 'center'}}>USDB offers the safest way to earn up to 20% on your stables. No risk. No surprises.</Typography>
-      <Paper className={`${style['graphContainer']} ${style['lightBG']}`} sx={{my: '2em'}} elevation={0}>
-        <Graph />
-      </Paper>
+      <Headline title={simpleSafe.title} subtitle={simpleSafe.subtitle} />
+      <Graph />
       <Grid container spacing={2} maxWidth="md">
         <Grid item xs={12} md={4}>
           <Paper className={`${style['infoIcon']} ${style['lightBG']}`} elevation={0}>
@@ -60,18 +69,14 @@ export const TradFi = (props: TradfiProps): JSX.Element => {
         </Grid>
       </Grid>
     </Box>
-    <Box className={`${style['tradFiBlock']} flexCenterCol`}>
-      <Box sx={{height: '10em'}}/>
-      <Typography variant="h1">Get started today</Typography>
-      <Typography variant="h2" maxWidth="sm" sx={{textAlign: 'center'}}>Lorem ipsum dolor sit amet, conctetur adipiscing elit. Etiam auctor commodo.</Typography>
-    </Box>
+    <Box sx={{height: '10em'}}/>
+    <Headline title={getStarted.title} subtitle={getStarted.subtitle} />
     <DepositChoice id="get-started"/>
     <Outlet />
     <Faq />
     <Box className={`${style['tradFiBlock']} flexCenterCol`}>
       <Box sx={{height: '10em'}}/>
-      <Typography variant="h1">Simple & safe returns</Typography>
-      <Typography variant="h2" maxWidth="sm" sx={{textAlign: 'center'}}>USDB offers the safest way to earn up to 20% on your stables. No risk. No surprises.</Typography>
+      <Headline title={simpleSafe.title} subtitle={simpleSafe.subtitle} />
     </Box>
     <Box className={`${style['tradFiBlock']} flexCenterCol`} sx={{paddingBottom: '4em'}}>
       <Logo />
