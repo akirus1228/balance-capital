086e08e70a2008a9c5a7672e874fa42fc0a5fca4
Cory Sougstad <cory@hau.me>
2022-03-23 10:39:52 -0700

    Working on theme aware image component

---
+++ libs/shared/images/src/lib/theme-image/theme-image.tsx
@@ -0,0 +1,13 @@
+import * from '../images/dark';
+import * from '../images/light';
+
+interface ThemeImageProps {
+    image: string
+}
+
+export const ThemeImage = (props: ThemeImageProps): JSX.Element => {
+
+    return(
+        <img src={} alt="" />
+    )
+}
