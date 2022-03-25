30c64b9b01f2e8f1182fc7eae7308570b367b2f8
pwntr0n <pwntr0n@fantohm.com>
2022-03-25 10:31:51 +0100

    enabled only tradfi for now
    Signed-off-by: pwntr0n <pwntr0n@fantohm.com>

--- apps/usdb/src/app/pages/home/icon-grid/icon-grid.tsx
+++ apps/usdb/src/app/pages/home/icon-grid/icon-grid.tsx
@@ -7,22 +7,25 @@ export const IconGrid = (): JSX.Element  => {
   return (
     <Grid container rowSpacing={6} className={style['productGrid']}>
       <Grid item md={4} xs={6}>
-        <IconLink title="Staking" icon={WalletIcon} link="/staking"/>
-      </Grid>
-      <Grid item md={4} xs={6}>
         <IconLink title="TradFi" icon={TradFiIcon} link="/trad-fi"/>
       </Grid>
       <Grid item md={4} xs={6}>
-        <IconLink title="xFHM" icon={xFhmIcon} link="/xfhm"/>
+        {/*<IconLink title="Staking" icon={WalletIcon} link="/staking" />*/}
+        <IconLink title="Staking" icon={WalletIcon} />
       </Grid>
       <Grid item md={4} xs={6}>
-        <IconLink title="Mint USDB" icon={MintIcon} link="/mint"/>
+        {/*<IconLink title="xFHM" icon={xFhmIcon} link="/xfhm"/>*/}
+        <IconLink title="xFHM" icon={xFhmIcon} />
+      </Grid>
+      <Grid item md={4} xs={6}>
+        {/*<IconLink title="Mint USDB" icon={MintIcon} link="/mint"/>*/}
+        <IconLink title="Mint USDB" icon={MintIcon} />
       </Grid>
       <Grid item md={4} xs={6}>
         <IconLink title="USDBank" icon={BankIcon}/>
       </Grid>
       <Grid item md={4} xs={6}>
-        <IconLink title="TBA" icon={NFTsIcon} />
+        <IconLink title="NFT" icon={NFTsIcon} />
       </Grid>
     </Grid>
   );
