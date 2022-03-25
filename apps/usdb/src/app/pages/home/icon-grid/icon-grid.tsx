9303f7e1aeacd847e09948f89dfb0badf63f448a
Cory Sougstad <cory@hau.me>
2022-03-24 17:22:48 -0700

    Add new homepage icons. Change default project to usdb.

--- apps/usdb/src/app/pages/home/icon-grid/icon-grid.tsx
+++ apps/usdb/src/app/pages/home/icon-grid/icon-grid.tsx
@@ -1,7 +1,7 @@
 import { Grid } from '@mui/material';
 import IconLink from '../../../components/icon-link/icon-link';
 import style from './icon-grid.module.scss';
-import { WalletIcon, TradFiIcon, BankIcon, NFTsIcon, DoughnutChartIcon, ShieldIcon } from '@fantohm/shared/images';
+import { WalletIcon, TradFiIcon, BankIcon, NFTsIcon, xFhmIcon, MintIcon } from '@fantohm/shared/images';
 
 export const IconGrid = (): JSX.Element  => {
   return (
@@ -13,16 +13,16 @@ export const IconGrid = (): JSX.Element  => {
         <IconLink title="TradFi" icon={TradFiIcon} link="/trad-fi"/>
       </Grid>
       <Grid item md={4} xs={6}>
+        <IconLink title="xFHM" icon={xFhmIcon} link="/xfhm"/>
+      </Grid>
+      <Grid item md={4} xs={6}>
+        <IconLink title="Mint USDB" icon={MintIcon} link="/mint"/>
+      </Grid>
+      <Grid item md={4} xs={6}>
         <IconLink title="USDBank" icon={BankIcon}/>
       </Grid>
       <Grid item md={4} xs={6}>
         <IconLink title="TBA" icon={NFTsIcon} />
-      </Grid>
-      <Grid item md={4} xs={6}>
-        <IconLink title="TBA" icon={DoughnutChartIcon} />
-      </Grid>
-      <Grid item md={4} xs={6}>
-        <IconLink title="TBA" icon={ShieldIcon} />
       </Grid>
     </Grid>
   );
