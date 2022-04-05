a3aabd42a504e7f88f9b22a993f296f6231ce655
danny-jin <icarus96119@gmail.com>
2022-04-05 16:55:58 +0900

    update: new bond address on fantom network

--- libs/shared/web3/src/lib/helpers/all-bonds.ts
+++ libs/shared/web3/src/lib/helpers/all-bonds.ts
@@ -135,7 +135,7 @@ export const singleSided = new StableBond({
   paymentToken: PaymentToken.FHM,
   networkAddrs: {
     [NetworkIds.FantomOpera]: {
-      bondAddress: "0x9D2141a3BfDbe0f9a948B993f9a70B5f9C9D17f9",
+      bondAddress: "0x5Cb0e53a90E0D6D27eDC2Ea667E61C536F9832ba",
       reserveAddress: "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E",
       // bondAddress: "0x6F3D5858BBC95553DfEeFda4886Ea20E933535fA",
       // bondAddress: "0xbA28476fc4EdAd909fA13A7048Ef432311B4680c",
