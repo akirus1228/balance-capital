6a729dec078a7673a6234d297a279c8818f62bc1
Cory Sougstad <cory@hau.me>
2022-04-25 22:12:36 -0700

    Minor cleanup, prep for tomorrow

--- apps/nft-market/src/app/components/asset-list/asset-list.tsx
+++ apps/nft-market/src/app/components/asset-list/asset-list.tsx
@@ -13,10 +13,11 @@ export const AssetList = (props: AssetListProps): JSX.Element => {
   return (
     <div>
       {assets.map((asset: Asset, index: number) => (
-        <div key={`asset-${index}`}>
-          collection: {asset.collection.name}
-          id: {asset.token_id}
-          <img src={asset.image_url} alt={asset.name}/>
+        <div key={`asset-${index}`} style={{ borderBottom: "1px solid #000" }}>
+          <span>collection</span>: <span>{asset.collection.name}</span>
+          <br />
+          <span>id</span>: <span>{asset.token_id}</span>
+          {asset.image_url && <img src={asset.image_url} alt={asset.name} />}
         </div>
       ))}
     </div>
