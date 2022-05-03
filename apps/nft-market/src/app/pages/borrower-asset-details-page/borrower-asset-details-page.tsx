4ef07a7863391c42002876cf572d537ee0e57576
Cory Sougstad <cory@hau.me>
2022-05-02 22:34:38 -0700

    Create additional asset and listing components. Populate borrower page.

--- apps/nft-market/src/app/pages/borrower-asset-details-page/borrower-asset-details-page.tsx
+++ apps/nft-market/src/app/pages/borrower-asset-details-page/borrower-asset-details-page.tsx
@@ -1,11 +1,11 @@
 import { Asset } from "@fantohm/shared-web3";
-import { Avatar, Box, Chip, Container, Grid, Skeleton, Typography } from "@mui/material";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { RootState } from "../../store";
-import style from "./borrower-asset-details-page.module.scss";
-import tmpAvatar from "../../../assets/images/temp-avatar.png";
+
+import AssetDetails from "../../components/asset-details/asset-details";
+import BorrowerLoanDetails from "../../components/borrower-loan-details/borrower-loan-details";
 
 export const BorrowerAssetDetailsPage = (): JSX.Element => {
   const params = useParams();
@@ -22,83 +22,10 @@ export const BorrowerAssetDetailsPage = (): JSX.Element => {
   }, [wallet.assets]);
 
   return (
-    <Container>
-      {currentAsset && currentAsset.imageUrl ? (
-        <Grid container columnSpacing={5}>
-          <Grid item>
-            <Box
-              className={style["imgContainer"]}
-              sx={{
-                borderRadius: "30px",
-                overflow: "hidden",
-                height: "50vh",
-                width: "50vh",
-              }}
-            >
-              <img src={currentAsset.imageUrl} alt={currentAsset.name || "unknown"} />
-            </Box>
-          </Grid>
-          <Grid item>
-            <Box sx={{ display: "flex", flexDirection: "column" }}>
-              <Typography>{currentAsset.name}</Typography>
-              <h1>{currentAsset.name}</h1>
-            </Box>
-            <Box sx={{ display: "flex", flexDirection: "row" }}>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  background: "#FFF",
-                  borderRadius: "30px",
-                  paddingTop: "5em",
-                  px: "20px",
-                }}
-              >
-                <Box sx={{ position: "absolute" }}>
-                  <Chip
-                    sx={{
-                      position: "relative",
-                      top: "15px",
-                      left: "20px",
-                      zIndex: 10,
-                    }}
-                    label={currentAsset.status || "Unlisted"}
-                  />
-                  <Chip
-                    sx={{
-                      position: "relative",
-                      top: "15px",
-                      left: "20px",
-                      zIndex: 10,
-                    }}
-                    label={currentAsset.mediaType || "Art"}
-                  />
-                </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <Box>
-                    <Avatar src={tmpAvatar} />
-                    <Typography>Owner</Typography>
-                    <Typography>You</Typography>
-                  </Box>
-                  <Box>
-                    <Typography>Listed</Typography>
-                    <Typography>14 hours ago</Typography>
-                  </Box>
-                </Box>
-              </Box>
-            </Box>
-          </Grid>
-        </Grid>
-      ) : (
-        <Skeleton variant="rectangular"></Skeleton>
-      )}
-    </Container>
+    <>
+      <AssetDetails asset={currentAsset} />
+      <BorrowerLoanDetails asset={currentAsset} sx={{ mt: "3em" }} />
+    </>
   );
 };
 
