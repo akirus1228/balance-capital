f718c139452b81467a2890fe7dfc7d49a0d5dfce
nd45 <90220677+nd45@users.noreply.github.com>
2022-05-06 07:22:06 -0700

    Add disclaimers (#241)
    * add disclaimers for contract interactions
    
    * add disclaimers for contract interactions

--- libs/shared/helpers/src/lib/browser.ts
+++ libs/shared/helpers/src/lib/browser.ts
@@ -1,6 +1,12 @@
+import { JsonRpcProvider } from "@ethersproject/providers";
+
 /**
  * determine if in IFrame for Ledger Live
  */
 export const isIframe = () => {
   return window.location !== window.parent.location;
+};
+
+export const sign = async (provider: JsonRpcProvider) => {
+  return await provider.getSigner().signMessage("message");
 };
