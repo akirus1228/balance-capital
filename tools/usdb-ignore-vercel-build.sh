de25844426aae1222423a347cb3ae92d01bae160
Cory Sougstad <cory@hau.me>
2022-05-10 16:38:25 -0700

    Fix other two scripts. Test usdb build

--- tools/usdb-ignore-vercel-build.sh
+++ tools/usdb-ignore-vercel-build.sh
@@ -7,8 +7,8 @@ NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nr
 TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
 # Install @nrwl/workspace in order to run the affected command
-npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline
-npm install -D typescript@$TS_VERSION --prefer-offline
+yarn add @nrwl/workspace@$NX_VERSION -D
+yarn add typescript@$TS_VERSION -De
 
 # Run the affected command, comparing latest commit to the one before that
 npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
