245a0fe6295a66e810d96a20526fdff226741199
Cory Sougstad <cory@hau.me>
2022-05-11 11:03:29 -0700

    Working asset listing

--- apps/nft-market/src/app/pages/lend-page/lend-page.tsx
+++ apps/nft-market/src/app/pages/lend-page/lend-page.tsx
@@ -1,29 +1,45 @@
 import { useWeb3Context } from "@fantohm/shared-web3";
 import { Container } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AssetList from "../../components/asset-list/asset-list";
-import { RootState } from "../../store";
-import { loadListings } from "../../store/reducers/listing-slice";
+import store, { RootState } from "../../store";
+import {
+  getListingState,
+  Listings,
+  loadListings,
+} from "../../store/reducers/listing-slice";
+import { selectListingByStatus } from "../../store/selectors/listing-selectors";
+import {
+  Asset,
+  BackendLoadingStatus,
+  Listing,
+  ListingStatus,
+} from "../../types/backend-types";
 import style from "./lend-page.module.scss";
 
 export const LendPage = (): JSX.Element => {
   const dispatch = useDispatch();
+  const [assets, setAssets] = useState([] as Asset[]);
   const { chainId, address } = useWeb3Context();
-  const assetState = useSelector((state: RootState) => state.assets);
-  const listings = useSelector((state: RootState) => state.listings.listings);
+  const listingState = getListingState(store.getState());
+  const listings: Listing[] = selectListingByStatus(listingState, ListingStatus.Listed);
 
   // Load assets and nfts in current wallet
   useEffect(() => {
-    if (assetState.assetStatus !== "loading" && assetState.nextOpenseaLoad < Date.now()) {
+    if (listingState.listingsLoadStatus !== BackendLoadingStatus.loading) {
       dispatch(loadListings({ queryParams: { skip: 0, take: 50 } }));
     }
-  }, [chainId, address, assetState.assetStatus]);
+  }, [chainId, address]);
+
+  useEffect(() => {
+    setAssets(listings.map((listing: Listing) => listing.asset));
+  }, [JSON.stringify(listings)]);
 
   return (
     <Container className={style["borrowPageContainer"]} maxWidth={`xl`}>
       <h1>Explore loan requests</h1>
-      <AssetList sx={{ mt: "2em" }} assets={assetState.assets} />
+      <AssetList sx={{ mt: "2em" }} assets={assets} />
     </Container>
   );
 };
--- apps/nft-market/src/app/store/selectors/listing-selectors.ts
+++ apps/nft-market/src/app/store/selectors/listing-selectors.ts
@@ -1,12 +1,12 @@
 import { createSelector } from "@reduxjs/toolkit";
-import { Asset, Listing } from "../../types/backend-types";
+import { Asset, Listing, ListingStatus } from "../../types/backend-types";
 import { Listings, ListingState } from "../reducers/listing-slice";
 
 const selectListings: (listingState: ListingState) => Listings = (
   listingState: ListingState
 ) => listingState.listings;
-const selectListingAsset = (listingState: ListingState, asset: Asset) => asset;
 
+const selectListingAsset = (listingState: ListingState, asset: Asset) => asset;
 export const selectListingFromAsset = createSelector(
   [selectListings, selectListingAsset],
   (listings, asset) => {
@@ -20,5 +20,16 @@ export const selectListingFromAsset = createSelector(
     } else {
       return {} as Listing;
     }
+  }
+);
+
+const selectListingStatus = (listingState: ListingState, status: ListingStatus) => status;
+export const selectListingByStatus = createSelector(
+  [selectListings, selectListingStatus],
+  (listings, status): Listing[] => {
+    const matches = Object.entries(listings).filter(
+      ([listingId, listing]) => listing.status === status
+    );
+    return matches.map(([listingId, listing]) => listing);
   }
 );
